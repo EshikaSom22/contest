4b66923821c2ac1266cc28dcefc222227bd8da7f
eshika <eshika22som2003@gmail.com>
2022-09-27 12:41:31 +0530

    dada go

--- ques3.js
+++ ques3.js
@@ -1,10 +1,6 @@
-let names=['Tulsi','Subhradeep','Suvam','Akash'];
 let streams=['CSE','ECE','CSE','IT'];
-let newArr=(names,streams)=>{
-    let arr=[];
-    for(var i=0;i<names.length;i++){
-arr[i]=`{stream: "${streams[i]}", name: "${names[i]}"}`;
-}
-return arr;
-}
-console.log(newArr(names,streams));
+let names=['Tulsi','Subhradeep','Suvam','Akash'];
+let newArr=streams.map(function(value,index){
+    return {stream:value,name:names[index]}
+});
+console.log(newArr);
